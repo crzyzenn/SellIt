30e5a28e8ef46df64937d526e7a935799e7e0d7f
crzyzenn <zenishshr@gmail.com>
2021-04-07 12:09:55 +0545

    Touchables on images, add prettier config

--- App.js
+++ App.js
@@ -1,20 +1,54 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+} from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Hello World</Text>
+      <TouchableNativeFeedback onPress={() => console.log('Image touched.')}>
+        <View
+          style={{ width: 200, height: 70, backgroundColor: 'dodgerblue' }}
+        ></View>
+      </TouchableNativeFeedback>
+      <TouchableHighlight onPress={() => console.log('Image touched.')}>
+        <Image
+          fadeDuration={1000}
+          source={{
+            width: 200,
+            height: 300,
+            uri: 'https://picsum.photos/200/300',
+          }}
+        />
+      </TouchableHighlight>
+      <TouchableOpacity onPress={() => console.log('Image touched.')}>
+        <Image
+          fadeDuration={1000}
+          source={{
+            width: 200,
+            height: 300,
+            uri: 'https://picsum.photos/200/300',
+          }}
+        />
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
